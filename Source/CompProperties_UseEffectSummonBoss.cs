c6acf666a78ef518e2df698a97842fdc97cfb010
Neversalimus <neversalim@yandex.ru>
2026-04-11 18:13:26 +0300

    Update CompProperties_UseEffectSummonBoss.cs

--- Source/CompProperties_UseEffectSummonBoss.cs
+++ Source/CompProperties_UseEffectSummonBoss.cs
@@ -5,9 +5,9 @@ namespace AbyssalProtocol
 {
     public class CompProperties_UseEffectSummonBoss : CompProperties_UseEffect
     {
-        public string pawnKindDefName = "SpaceSoldier";
-        public string bossLabel = "Archon of Rupture";
-        public int spawnPoints = 650;
+        public string pawnKindDefName = "ABY_ArchonBeast";
+        public string bossLabel = "Archon Beast";
+        public int spawnPoints = 900;
 
         public CompProperties_UseEffectSummonBoss()
         {
