19aa7edf03ba22ac27a646e90825b53d38c02a95
Neversalimus <neversalim@yandex.ru>
2026-04-11 18:45:39 +0300

    Create DeathActionWorker_DropUltraPlasmaRifle.cs

---
+++ Source/DeathActionWorker_DropUltraPlasmaRifle.cs
@@ -0,0 +1,26 @@
+using RimWorld;
+using Verse;
+
+namespace AbyssalProtocol
+{
+    public class DeathActionWorker_DropUltraPlasmaRifle : DeathActionWorker
+    {
+        public override void PawnDied(Corpse corpse)
+        {
+            base.PawnDied(corpse);
+
+            if (corpse == null || corpse.Map == null)
+                return;
+
+            ThingDef rifleDef = DefDatabase<ThingDef>.GetNamedSilentFail("ABY_UltraPlasmaRifle");
+            if (rifleDef == null)
+                return;
+
+            Thing rifle = ThingMaker.MakeThing(rifleDef);
+            if (rifle == null)
+                return;
+
+            GenPlace.TryPlaceThing(rifle, corpse.Position, corpse.Map, ThingPlaceMode.Near);
+        }
+    }
+}
