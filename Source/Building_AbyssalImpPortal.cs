7bb9ccc9c81be1e0745c60f62790bb3edd80b4bf
Neversalimus <neversalim@yandex.ru>
2026-04-12 18:56:08 +0300

    Update Building_AbyssalImpPortal.cs

--- Source/Building_AbyssalImpPortal.cs
+++ Source/Building_AbyssalImpPortal.cs
@@ -51,7 +51,7 @@ namespace AbyssalProtocol
             Scribe_Values.Look(ref seed, "seed", 0);
         }
 
-        public override void Tick()
+        protected override void Tick()
         {
             base.Tick();
 
