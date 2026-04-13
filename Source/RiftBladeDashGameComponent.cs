c6821079968cb8550bb3755591ebe352226bd113
Neversalimus <neversalim@yandex.ru>
2026-04-13 15:45:11 +0300

    Add files via upload

--- Source/RiftBladeDashGameComponent.cs
+++ Source/RiftBladeDashGameComponent.cs
@@ -61,7 +61,7 @@ namespace AbyssalProtocol
                     continue;
                 }
 
-                List<Pawn> pawns = map.mapPawns.AllPawnsSpawned;
+                IReadOnlyList<Pawn> pawns = map.mapPawns.AllPawnsSpawned;
                 for (int j = 0; j < pawns.Count; j++)
                 {
                     Pawn pawn = pawns[j];
