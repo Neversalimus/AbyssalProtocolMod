372cc5055dd6dbba60092f8f81100e7732ca71f3
Neversalimus <neversalim@yandex.ru>
2026-04-13 21:34:50 +0300

    Add files via upload

---
+++ Source/Thing_HeraldAnalysisPacket.cs
@@ -0,0 +1,34 @@
+using Verse;
+
+namespace AbyssalProtocol
+{
+    public class Thing_HeraldAnalysisPacket : ThingWithComps
+    {
+        private bool pendingResolve = true;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            pendingResolve = !respawningAfterLoad;
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (!pendingResolve || MapHeld == null || Destroyed)
+            {
+                return;
+            }
+
+            pendingResolve = false;
+            ABY_HeraldFragmentAnalysisUtility.ResolveAnalysisPacket(this);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref pendingResolve, "pendingResolve", true);
+        }
+    }
+}
