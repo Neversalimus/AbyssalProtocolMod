796ed78508fa9161d04d173894bd15b06e03afaf
Neversalimus <neversalim@yandex.ru>
2026-04-18 02:54:24 +0300

    Add files via upload

--- Source/Building_ABY_ReactorSaintCocoon.cs
+++ Source/Building_ABY_ReactorSaintCocoon.cs
@@ -4,7 +4,7 @@ using Verse;
 
 namespace AbyssalProtocol
 {
-    public sealed class Building_ABY_ReactorSaintCocoon : Building
+    public sealed class Building_ABY_ReactorSaintCocoon : Building_ABY_SkyfallerVesselBase
     {
         private const string BossPawnKindDefName = "ABY_ReactorSaint";
         private const string BossLabel = "Infernal Reactor Saint";
@@ -14,235 +14,81 @@ namespace AbyssalProtocol
         private const string CocoonTexPath = "Things/VFX/ReactorSaintArrival/ABY_ReactorSaintCocoon";
         private const string CocoonShadowTexPath = "Things/VFX/ReactorSaintArrival/ABY_ReactorSaintCocoon_Shadow";
 
-        private const int ReleaseDelayTicks = 834;
-        private const int PostReleaseTicks = 417;
-        private const int LaunchDurationTicks = 84;
+        protected override int ReleaseDelayTicks => 834;
+        protected override int PostReleaseTicks => 417;
+        protected override int LaunchDurationTicks => 84;
 
-        private const float ImpactExplosionRadius = 3.9f;
-        private const int ImpactExplosionDamage = 28;
-        private const float ImpactExplosionArmorPenetration = 0.18f;
+        protected override float ImpactExplosionRadius => 3.9f;
+        protected override int ImpactExplosionDamage => 28;
+        protected override float ImpactExplosionArmorPenetration => 0.18f;
 
-        private const float BodyScaleX = 15.95f;
-        private const float BodyScaleZ = 23.10f;
-        private const float ShadowScale = 23.10f;
-        private const float LaunchNorthDrift = 58.00f;
-        private const float LaunchSideDrift = 0.30f;
-        private const float LaunchAltitudeBoost = 4.20f;
-        private const float LaunchBodyScaleEnd = 0.98f;
-        private const float LaunchShadowScaleEnd = 0.42f;
-        private const float ShadowAlpha = 0.62f;
+        protected override string BodyTexPath => CocoonTexPath;
+        protected override string ShadowTexPath => CocoonShadowTexPath;
+        protected override float BodyScaleX => 15.95f;
+        protected override float BodyScaleZ => 23.10f;
+        protected override float ShadowScale => 23.10f;
+        protected override float ShadowAlpha => 0.62f;
 
-        private static readonly Material CocoonMat = MaterialPool.MatFrom(CocoonTexPath, ShaderDatabase.Cutout, Color.white);
-        private static readonly Material CocoonShadowMat = MaterialPool.MatFrom(CocoonShadowTexPath, ShaderDatabase.TransparentPostLight, new Color(1f, 1f, 1f, ShadowAlpha));
+        protected override float LaunchNorthDrift => 58.00f;
+        protected override float LaunchSideDrift => 0.30f;
+        protected override float LaunchAltitudeBoost => 4.20f;
+        protected override float LaunchBodyScaleEnd => 0.98f;
+        protected override float LaunchShadowScaleEnd => 0.42f;
 
-        private int ticksSinceImpact;
-        private bool bossReleased;
-        private bool releaseFailedPermanently;
-        private bool impactProcessed;
-        private bool launching;
-        private int launchTicks;
-
-        public override void ExposeData()
+        protected override void TickDormantVessel()
         {
-            base.ExposeData();
-            Scribe_Values.Look(ref ticksSinceImpact, "ticksSinceImpact", 0);
-            Scribe_Values.Look(ref bossReleased, "bossReleased", false);
-            Scribe_Values.Look(ref releaseFailedPermanently, "releaseFailedPermanently", false);
-            Scribe_Values.Look(ref impactProcessed, "impactProcessed", false);
-            Scribe_Values.Look(ref launching, "launching", false);
-            Scribe_Values.Look(ref launchTicks, "launchTicks", 0);
-        }
-
-        public override void SpawnSetup(Map map, bool respawningAfterLoad)
-        {
-            base.SpawnSetup(map, respawningAfterLoad);
-
-            if (!respawningAfterLoad && !impactProcessed)
-            {
-                impactProcessed = true;
-                TriggerImpactEffects();
-            }
-        }
-
-        protected override void Tick()
-        {
-            base.Tick();
-
-            if (Map == null || Destroyed)
-            {
-                return;
-            }
-
-            if (launching)
-            {
-                TickLaunching();
-                return;
-            }
-
-            ticksSinceImpact++;
-
-            if (!bossReleased)
-            {
-                TickDormantCocoon();
-
-                if (!releaseFailedPermanently && ticksSinceImpact >= ReleaseDelayTicks)
-                {
-                    TryReleaseBoss();
-                }
-
-                return;
-            }
-
-            TickSpentCocoon();
-
-            if (ticksSinceImpact >= ReleaseDelayTicks + PostReleaseTicks)
-            {
-                BeginLaunch();
-            }
-        }
-
-        protected override void DrawAt(Vector3 drawLoc, bool flip = false)
-        {
-            Vector3 bodyLoc = drawLoc;
-            Vector3 shadowLoc = drawLoc;
-            float bodyScaleX = BodyScaleX;
-            float bodyScaleZ = BodyScaleZ;
-            float shadowScale = ShadowScale;
-            float shadowAlpha = ShadowAlpha;
-
-            ApplyLaunchTransform(ref bodyLoc, ref shadowLoc, ref bodyScaleX, ref bodyScaleZ, ref shadowScale, ref shadowAlpha);
-
-            DrawCocoonShadow(shadowLoc, shadowScale, shadowAlpha);
-            DrawCocoonBody(bodyLoc, bodyScaleX, bodyScaleZ);
-        }
-
-
-        private void ApplyLaunchTransform(ref Vector3 bodyLoc, ref Vector3 shadowLoc, ref float bodyScaleX, ref float bodyScaleZ, ref float shadowScale, ref float shadowAlpha)
-        {
-            if (launching)
-            {
-                float progress = Mathf.Clamp01(launchTicks / (float)LaunchDurationTicks);
-                float eased = 1f - Mathf.Pow(1f - progress, 2.2f);
-
-                bodyLoc.x += eased * LaunchSideDrift;
-                bodyLoc.z += eased * LaunchNorthDrift;
-                bodyLoc.y = AltitudeLayer.BuildingOnTop.AltitudeFor() + 0.04f + eased * LaunchAltitudeBoost;
-
-                shadowLoc.x += eased * (LaunchSideDrift * 0.14f);
-                shadowLoc.z += eased * (LaunchNorthDrift * 0.10f);
-                shadowLoc.y = AltitudeLayer.Shadows.AltitudeFor();
-
-                float bodyScale = Mathf.Lerp(1f, LaunchBodyScaleEnd, eased);
-                float shadowScaleFactor = Mathf.Lerp(1f, LaunchShadowScaleEnd, eased);
-                bodyScaleX *= bodyScale;
-                bodyScaleZ *= bodyScale;
-                shadowScale *= shadowScaleFactor;
-                shadowAlpha *= 1f - eased;
-            }
-            else
-            {
-                bodyLoc.y = AltitudeLayer.BuildingOnTop.AltitudeFor() + 0.04f;
-                shadowLoc.y = AltitudeLayer.Shadows.AltitudeFor();
-            }
-        }
-
-        private Vector3 GetCurrentBodyDrawPos()
-        {
-            Vector3 bodyLoc = DrawPos;
-            Vector3 shadowLoc = DrawPos;
-            float bodyScaleX = BodyScaleX;
-            float bodyScaleZ = BodyScaleZ;
-            float shadowScale = ShadowScale;
-            float shadowAlpha = ShadowAlpha;
-            ApplyLaunchTransform(ref bodyLoc, ref shadowLoc, ref bodyScaleX, ref bodyScaleZ, ref shadowScale, ref shadowAlpha);
-            return bodyLoc;
-        }
-        private void DrawCocoonBody(Vector3 loc, float scaleX, float scaleZ)
-        {
-            Matrix4x4 matrix = Matrix4x4.identity;
-            matrix.SetTRS(loc, Quaternion.identity, new Vector3(scaleX, 1f, scaleZ));
-            Graphics.DrawMesh(MeshPool.plane10, matrix, CocoonMat, 0);
-        }
-
-        private void DrawCocoonShadow(Vector3 loc, float scale, float alpha)
-        {
-            if (alpha <= 0.01f)
-            {
-                return;
-            }
-
-            Material shadowMat = MaterialPool.MatFrom(CocoonShadowTexPath, ShaderDatabase.TransparentPostLight, new Color(1f, 1f, 1f, alpha));
-            Matrix4x4 matrix = Matrix4x4.identity;
-            matrix.SetTRS(loc, Quaternion.identity, new Vector3(scale, 1f, scale));
-            Graphics.DrawMesh(MeshPool.plane10, matrix, shadowMat, 0);
-        }
-
-        private void TriggerImpactEffects()
-        {
-            if (Map == null)
-            {
-                return;
-            }
-
-            FleckMaker.ThrowLightningGlow(DrawPos, Map, 2.8f);
-            FleckMaker.ThrowHeatGlow(Position, Map, 2.1f);
-            FleckMaker.ThrowMicroSparks(DrawPos, Map);
-            FilthMaker.TryMakeFilth(Position, Map, ThingDefOf.Filth_Ash, 3);
-            GenExplosion.DoExplosion(Position, Map, ImpactExplosionRadius, DamageDefOf.Burn, this, ImpactExplosionDamage, ImpactExplosionArmorPenetration);
-        }
-
-        private void TickDormantCocoon()
-        {
-            if (ticksSinceImpact % 11 == 0)
+            if (TicksSinceImpact % 11 == 0)
             {
                 FleckMaker.ThrowMicroSparks(DrawPos, Map);
             }
 
-            if (ticksSinceImpact % 30 == 0)
+            if (TicksSinceImpact % 30 == 0)
             {
                 FleckMaker.ThrowHeatGlow(Position, Map, 1.20f);
             }
 
-            if (ticksSinceImpact % 60 == 0)
+            if (TicksSinceImpact % 60 == 0)
             {
                 FleckMaker.ThrowLightningGlow(DrawPos, Map, 1.65f);
                 FilthMaker.TryMakeFilth(Position, Map, ThingDefOf.Filth_Ash, 1);
             }
         }
 
-        private void TickSpentCocoon()
+        protected override void TickSpentVessel()
         {
-            if (ticksSinceImpact % 24 == 0)
+            if (TicksSinceImpact % 24 == 0)
             {
                 FleckMaker.ThrowMicroSparks(DrawPos, Map);
             }
 
-            if (ticksSinceImpact % 52 == 0)
+            if (TicksSinceImpact % 52 == 0)
             {
                 FleckMaker.ThrowLightningGlow(DrawPos, Map, 1.10f);
             }
         }
 
-        private void TryReleaseBoss()
+        protected override bool TryReleasePayload(out bool permanentFailure)
         {
+            permanentFailure = false;
+
             PawnKindDef kindDef = DefDatabase<PawnKindDef>.GetNamedSilentFail(BossPawnKindDefName);
             Faction faction = AbyssalBossSummonUtility.ResolveHostileFaction();
 
             if (kindDef == null || faction == null)
             {
-                releaseFailedPermanently = true;
+                permanentFailure = true;
                 Log.Warning("[AbyssalProtocol] Reactor Saint cocoon could not resolve boss kind or hostile faction.");
-                return;
+                return false;
             }
 
             if (!AbyssalBossSummonUtility.TryGenerateBoss(Map, kindDef, faction, BossLabel, out Pawn pawn, out string failReason))
             {
-                if (ticksSinceImpact % 60 == 0 && !failReason.NullOrEmpty())
+                if (TicksSinceImpact % 60 == 0 && !failReason.NullOrEmpty())
                 {
                     Log.Warning("[AbyssalProtocol] Reactor Saint cocoon failed to generate boss: " + failReason);
                 }
 
-                return;
+                return false;
             }
 
             IntVec3 releaseCell = FindReleaseCell();
@@ -256,103 +102,67 @@ namespace AbyssalProtocol
                 CompletionLetterLabelKey,
                 CompletionLetterDescKey);
 
-            bossReleased = true;
             FleckMaker.ThrowLightningGlow(DrawPos, Map, 2.30f);
             FleckMaker.ThrowHeatGlow(Position, Map, 1.55f);
             FilthMaker.TryMakeFilth(Position, Map, ThingDefOf.Filth_Ash, 2);
+            return true;
         }
 
-        private IntVec3 FindReleaseCell()
+        protected override void OnBeginLaunch()
         {
-            if (IsValidReleaseCell(Position))
-            {
-                return Position;
-            }
-
-            for (int i = 0; i < GenRadial.NumCellsInRadius(2.9f); i++)
-            {
-                IntVec3 candidate = Position + GenRadial.RadialPattern[i];
-                if (IsValidReleaseCell(candidate))
-                {
-                    return candidate;
-                }
-            }
-
-            return Position;
-        }
-
-        private bool IsValidReleaseCell(IntVec3 cell)
-        {
-            return cell.IsValid && cell.InBounds(Map) && cell.Standable(Map) && !cell.Fogged(Map);
-        }
-
-        private void BeginLaunch()
-        {
-            if (launching || Map == null || Destroyed)
-            {
-                return;
-            }
-
-            launching = true;
-            launchTicks = 0;
-
             FleckMaker.ThrowLightningGlow(DrawPos, Map, 2.40f);
             FleckMaker.ThrowHeatGlow(Position, Map, 1.55f);
             FleckMaker.ThrowMicroSparks(DrawPos, Map);
         }
 
-        private void TickLaunching()
+        protected override void TickLaunchFx()
         {
-            launchTicks++;
-
-            if (Map != null)
+            if (Map == null)
             {
-                float progress = Mathf.Clamp01(launchTicks / (float)LaunchDurationTicks);
-                Vector3 bodyPos = GetCurrentBodyDrawPos();
-                Vector3 exhaustCenter = bodyPos + new Vector3(0f, 0f, -4.40f);
-                Vector3 exhaustLeft = exhaustCenter + new Vector3(-3.10f, 0f, -0.45f);
-                Vector3 exhaustRight = exhaustCenter + new Vector3(3.10f, 0f, -0.45f);
-                float fireGlowSize = 1.55f + progress * 1.55f;
-                float smokeSize = 1.10f + progress * 1.30f;
-                float dustSize = 1.40f + progress * 1.35f;
-                float lightningGlow = 1.25f + progress * 1.65f;
-
-                if (launchTicks % 2 == 0)
-                {
-                    FleckMaker.ThrowMicroSparks(exhaustLeft, Map);
-                    FleckMaker.ThrowMicroSparks(exhaustRight, Map);
-                    FleckMaker.ThrowFireGlow(exhaustLeft, Map, fireGlowSize);
-                    FleckMaker.ThrowFireGlow(exhaustRight, Map, fireGlowSize);
-                }
-
-                if (launchTicks % 3 == 0)
-                {
-                    FleckMaker.ThrowSmoke(exhaustLeft, Map, smokeSize);
-                    FleckMaker.ThrowSmoke(exhaustRight, Map, smokeSize);
-                }
-
-                if (launchTicks % 4 == 0)
-                {
-                    Vector3 groundFxCenter = DrawPos + new Vector3(0f, 0f, -2.80f);
-                    FleckMaker.ThrowDustPuff(groundFxCenter + new Vector3(-2.20f, 0f, 0f), Map, dustSize);
-                    FleckMaker.ThrowDustPuff(groundFxCenter + new Vector3(2.20f, 0f, 0f), Map, dustSize);
-                    FleckMaker.ThrowHeatGlow(Position, Map, 1.35f + progress * 1.10f);
-                }
-
-                if (launchTicks % 6 == 0)
-                {
-                    FleckMaker.ThrowLightningGlow(exhaustCenter, Map, lightningGlow);
-                }
-
-                if (launchTicks % 10 == 0)
-                {
-                    FleckMaker.ThrowLightningGlow(bodyPos, Map, 1.65f + progress * 1.80f);
-                }
+                return;
             }
 
-            if (launchTicks >= LaunchDurationTicks)
+            float progress = LaunchProgress;
+            Vector3 bodyPos = GetCurrentBodyDrawPos();
+            Vector3 exhaustCenter = bodyPos + new Vector3(0f, 0f, -4.40f);
+            Vector3 exhaustLeft = exhaustCenter + new Vector3(-3.10f, 0f, -0.45f);
+            Vector3 exhaustRight = exhaustCenter + new Vector3(3.10f, 0f, -0.45f);
+            float fireGlowSize = 1.55f + progress * 1.55f;
+            float smokeSize = 1.10f + progress * 1.30f;
+            float dustSize = 1.40f + progress * 1.35f;
+            float lightningGlow = 1.25f + progress * 1.65f;
+            int launchTicks = Mathf.RoundToInt(progress * LaunchDurationTicks);
+
+            if (launchTicks % 2 == 0)
             {
-                Destroy(DestroyMode.Vanish);
+                FleckMaker.ThrowMicroSparks(exhaustLeft, Map);
+                FleckMaker.ThrowMicroSparks(exhaustRight, Map);
+                FleckMaker.ThrowFireGlow(exhaustLeft, Map, fireGlowSize);
+                FleckMaker.ThrowFireGlow(exhaustRight, Map, fireGlowSize);
+            }
+
+            if (launchTicks % 3 == 0)
+            {
+                FleckMaker.ThrowSmoke(exhaustLeft, Map, smokeSize);
+                FleckMaker.ThrowSmoke(exhaustRight, Map, smokeSize);
+            }
+
+            if (launchTicks % 4 == 0)
+            {
+                Vector3 groundFxCenter = DrawPos + new Vector3(0f, 0f, -2.80f);
+                FleckMaker.ThrowDustPuff(groundFxCenter + new Vector3(-2.20f, 0f, 0f), Map, dustSize);
+                FleckMaker.ThrowDustPuff(groundFxCenter + new Vector3(2.20f, 0f, 0f), Map, dustSize);
+                FleckMaker.ThrowHeatGlow(Position, Map, 1.35f + progress * 1.10f);
+            }
+
+            if (launchTicks % 6 == 0)
+            {
+                FleckMaker.ThrowLightningGlow(exhaustCenter, Map, lightningGlow);
+            }
+
+            if (launchTicks % 10 == 0)
+            {
+                FleckMaker.ThrowLightningGlow(bodyPos, Map, 1.65f + progress * 1.80f);
             }
         }
     }
---
+++ Source/Building_ABY_SkyfallerVesselBase.cs
@@ -0,0 +1,292 @@
+using System;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AbyssalProtocol
+{
+    public abstract class Building_ABY_SkyfallerVesselBase : Building
+    {
+        private int ticksSinceImpact;
+        private bool payloadReleased;
+        private bool releaseFailedPermanently;
+        private bool impactProcessed;
+        private bool launching;
+        private int launchTicks;
+
+        protected int TicksSinceImpact => ticksSinceImpact;
+        protected bool PayloadReleased => payloadReleased;
+        protected bool Launching => launching;
+        protected float LaunchProgress => Mathf.Clamp01(launchTicks / (float)Math.Max(1, LaunchDurationTicks));
+
+        protected virtual int ReleaseDelayTicks => 600;
+        protected virtual int PostReleaseTicks => 300;
+        protected virtual int LaunchDurationTicks => 60;
+
+        protected virtual float ImpactExplosionRadius => 0f;
+        protected virtual int ImpactExplosionDamage => 0;
+        protected virtual float ImpactExplosionArmorPenetration => 0f;
+
+        protected virtual string BodyTexPath => null;
+        protected virtual string ShadowTexPath => null;
+        protected virtual Shader BodyShader => ShaderDatabase.Cutout;
+        protected virtual Shader ShadowShader => ShaderDatabase.TransparentPostLight;
+        protected virtual float BodyScaleX => 1f;
+        protected virtual float BodyScaleZ => 1f;
+        protected virtual float ShadowScale => 1f;
+        protected virtual float ShadowAlpha => 0.62f;
+
+        protected virtual float LaunchNorthDrift => 0f;
+        protected virtual float LaunchSideDrift => 0f;
+        protected virtual float LaunchAltitudeBoost => 0f;
+        protected virtual float LaunchBodyScaleEnd => 1f;
+        protected virtual float LaunchShadowScaleEnd => 0.4f;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref ticksSinceImpact, "ticksSinceImpact", 0);
+            Scribe_Values.Look(ref payloadReleased, "payloadReleased", false);
+            Scribe_Values.Look(ref releaseFailedPermanently, "releaseFailedPermanently", false);
+            Scribe_Values.Look(ref impactProcessed, "impactProcessed", false);
+            Scribe_Values.Look(ref launching, "launching", false);
+            Scribe_Values.Look(ref launchTicks, "launchTicks", 0);
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            if (!respawningAfterLoad && !impactProcessed)
+            {
+                impactProcessed = true;
+                TriggerImpactEffects();
+                OnImpactProcessed();
+            }
+        }
+
+        protected override void Tick()
+        {
+            base.Tick();
+
+            if (Map == null || Destroyed)
+            {
+                return;
+            }
+
+            if (launching)
+            {
+                TickLaunching();
+                return;
+            }
+
+            ticksSinceImpact++;
+
+            if (!payloadReleased)
+            {
+                TickDormantVessel();
+
+                if (!releaseFailedPermanently && ticksSinceImpact >= ReleaseDelayTicks)
+                {
+                    bool permanentFailure;
+                    if (TryReleasePayload(out permanentFailure))
+                    {
+                        payloadReleased = true;
+                        OnPayloadReleased();
+                    }
+                    else if (permanentFailure)
+                    {
+                        releaseFailedPermanently = true;
+                        OnReleaseFailedPermanently();
+                    }
+                }
+
+                return;
+            }
+
+            TickSpentVessel();
+
+            if (ticksSinceImpact >= ReleaseDelayTicks + PostReleaseTicks)
+            {
+                BeginLaunch();
+            }
+        }
+
+        protected override void DrawAt(Vector3 drawLoc, bool flip = false)
+        {
+            Vector3 bodyLoc = drawLoc;
+            Vector3 shadowLoc = drawLoc;
+            float bodyScaleX = BodyScaleX;
+            float bodyScaleZ = BodyScaleZ;
+            float shadowScale = ShadowScale;
+            float shadowAlpha = ShadowAlpha;
+
+            ApplyLaunchTransform(ref bodyLoc, ref shadowLoc, ref bodyScaleX, ref bodyScaleZ, ref shadowScale, ref shadowAlpha);
+
+            DrawShadow(shadowLoc, shadowScale, shadowAlpha);
+            DrawBody(bodyLoc, bodyScaleX, bodyScaleZ);
+        }
+
+        protected virtual void TickDormantVessel()
+        {
+        }
+
+        protected virtual void TickSpentVessel()
+        {
+        }
+
+        protected virtual void TickLaunchFx()
+        {
+        }
+
+        protected virtual void OnPayloadReleased()
+        {
+        }
+
+        protected virtual void OnImpactProcessed()
+        {
+        }
+
+        protected virtual void OnBeginLaunch()
+        {
+        }
+
+        protected virtual void OnReleaseFailedPermanently()
+        {
+        }
+
+        protected abstract bool TryReleasePayload(out bool permanentFailure);
+
+        protected virtual IntVec3 FindReleaseCell(float searchRadius = 2.9f)
+        {
+            if (IsValidReleaseCell(Position))
+            {
+                return Position;
+            }
+
+            for (int i = 0; i < GenRadial.NumCellsInRadius(searchRadius); i++)
+            {
+                IntVec3 candidate = Position + GenRadial.RadialPattern[i];
+                if (IsValidReleaseCell(candidate))
+                {
+                    return candidate;
+                }
+            }
+
+            return Position;
+        }
+
+        protected virtual bool IsValidReleaseCell(IntVec3 cell)
+        {
+            return cell.IsValid && cell.InBounds(Map) && cell.Standable(Map) && !cell.Fogged(Map);
+        }
+
+        protected Vector3 GetCurrentBodyDrawPos()
+        {
+            Vector3 bodyLoc = DrawPos;
+            Vector3 shadowLoc = DrawPos;
+            float bodyScaleX = BodyScaleX;
+            float bodyScaleZ = BodyScaleZ;
+            float shadowScale = ShadowScale;
+            float shadowAlpha = ShadowAlpha;
+            ApplyLaunchTransform(ref bodyLoc, ref shadowLoc, ref bodyScaleX, ref bodyScaleZ, ref shadowScale, ref shadowAlpha);
+            return bodyLoc;
+        }
+
+        private void TriggerImpactEffects()
+        {
+            if (Map == null)
+            {
+                return;
+            }
+
+            FleckMaker.ThrowLightningGlow(DrawPos, Map, 2.8f);
+            FleckMaker.ThrowHeatGlow(Position, Map, 2.1f);
+            FleckMaker.ThrowMicroSparks(DrawPos, Map);
+            FilthMaker.TryMakeFilth(Position, Map, ThingDefOf.Filth_Ash, 3);
+
+            if (ImpactExplosionRadius > 0.1f && ImpactExplosionDamage > 0)
+            {
+                GenExplosion.DoExplosion(Position, Map, ImpactExplosionRadius, DamageDefOf.Burn, this, ImpactExplosionDamage, ImpactExplosionArmorPenetration);
+            }
+        }
+
+        private void BeginLaunch()
+        {
+            if (launching || Map == null || Destroyed)
+            {
+                return;
+            }
+
+            launching = true;
+            launchTicks = 0;
+            OnBeginLaunch();
+        }
+
+        private void TickLaunching()
+        {
+            launchTicks++;
+            TickLaunchFx();
+
+            if (launchTicks >= LaunchDurationTicks)
+            {
+                Destroy(DestroyMode.Vanish);
+            }
+        }
+
+        private void ApplyLaunchTransform(ref Vector3 bodyLoc, ref Vector3 shadowLoc, ref float bodyScaleX, ref float bodyScaleZ, ref float shadowScale, ref float shadowAlpha)
+        {
+            if (launching)
+            {
+                float progress = LaunchProgress;
+                float eased = 1f - Mathf.Pow(1f - progress, 2.2f);
+
+                bodyLoc.x += eased * LaunchSideDrift;
+                bodyLoc.z -= eased * LaunchNorthDrift;
+                bodyLoc.y = AltitudeLayer.BuildingOnTop.AltitudeFor() + 0.04f + eased * LaunchAltitudeBoost;
+
+                shadowLoc.x += eased * (LaunchSideDrift * 0.14f);
+                shadowLoc.z -= eased * (LaunchNorthDrift * 0.10f);
+                shadowLoc.y = AltitudeLayer.Shadows.AltitudeFor();
+
+                float bodyScale = Mathf.Lerp(1f, LaunchBodyScaleEnd, eased);
+                float shadowScaleFactor = Mathf.Lerp(1f, LaunchShadowScaleEnd, eased);
+                bodyScaleX *= bodyScale;
+                bodyScaleZ *= bodyScale;
+                shadowScale *= shadowScaleFactor;
+                shadowAlpha *= 1f - eased;
+            }
+            else
+            {
+                bodyLoc.y = AltitudeLayer.BuildingOnTop.AltitudeFor() + 0.04f;
+                shadowLoc.y = AltitudeLayer.Shadows.AltitudeFor();
+            }
+        }
+
+        private void DrawBody(Vector3 loc, float scaleX, float scaleZ)
+        {
+            if (BodyTexPath.NullOrEmpty())
+            {
+                return;
+            }
+
+            Material material = MaterialPool.MatFrom(BodyTexPath, BodyShader, Color.white);
+            Matrix4x4 matrix = Matrix4x4.identity;
+            matrix.SetTRS(loc, Quaternion.identity, new Vector3(scaleX, 1f, scaleZ));
+            Graphics.DrawMesh(MeshPool.plane10, matrix, material, 0);
+        }
+
+        private void DrawShadow(Vector3 loc, float scale, float alpha)
+        {
+            if (ShadowTexPath.NullOrEmpty() || alpha <= 0.01f)
+            {
+                return;
+            }
+
+            Material shadowMat = MaterialPool.MatFrom(ShadowTexPath, ShadowShader, new Color(1f, 1f, 1f, alpha));
+            Matrix4x4 matrix = Matrix4x4.identity;
+            matrix.SetTRS(loc, Quaternion.identity, new Vector3(scale, 1f, scale));
+            Graphics.DrawMesh(MeshPool.plane10, matrix, shadowMat, 0);
+        }
+    }
+}
