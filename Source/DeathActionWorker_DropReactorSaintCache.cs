7132811b6904335cdfd7a3f3bca84a6da06f7d63
Neversalimus <neversalim@yandex.ru>
2026-04-17 17:24:00 +0300

    Add files via upload

---
+++ Source/DeathActionWorker_DropReactorSaintCache.cs
@@ -0,0 +1,68 @@
+using System;
+using RimWorld;
+using Verse;
+using Verse.AI.Group;
+
+namespace AbyssalProtocol
+{
+    public class DeathActionWorker_DropReactorSaintCache : DeathActionWorker
+    {
+        private const int MinBonusResidueDrop = 14;
+        private const int MaxBonusResidueDrop = 20;
+
+        public override void PawnDied(Corpse corpse, Lord prevLord)
+        {
+            if (corpse == null || corpse.Map == null)
+            {
+                return;
+            }
+
+            Map map = corpse.Map;
+            IntVec3 cell = corpse.Position;
+            bool droppedAny = false;
+
+            droppedAny |= TrySpawnSingle(map, cell, "ABY_ReactorSaintCore");
+            droppedAny |= TrySpawnSingle(map, cell, "ComponentSpacer");
+
+            if (Rand.Chance(0.35f))
+            {
+                droppedAny |= TrySpawnSingle(map, cell, "ComponentSpacer");
+            }
+
+            ThingDef residueDef = DefDatabase<ThingDef>.GetNamedSilentFail("ABY_AbyssalResidue");
+            if (residueDef != null)
+            {
+                Thing residue = ThingMaker.MakeThing(residueDef);
+                if (residue != null)
+                {
+                    residue.stackCount = Math.Min(residueDef.stackLimit, Rand.RangeInclusive(MinBonusResidueDrop, MaxBonusResidueDrop));
+                    if (GenPlace.TryPlaceThing(residue, cell, map, ThingPlaceMode.Near))
+                    {
+                        droppedAny = true;
+                    }
+                }
+            }
+
+            if (droppedAny)
+            {
+                Messages.Message(
+                    "ABY_ReactorSaintRewardRecovered".Translate(),
+                    new TargetInfo(cell, map),
+                    MessageTypeDefOf.PositiveEvent,
+                    false);
+            }
+        }
+
+        private static bool TrySpawnSingle(Map map, IntVec3 nearCell, string defName)
+        {
+            ThingDef def = DefDatabase<ThingDef>.GetNamedSilentFail(defName);
+            if (def == null)
+            {
+                return false;
+            }
+
+            Thing thing = ThingMaker.MakeThing(def);
+            return thing != null && GenPlace.TryPlaceThing(thing, nearCell, map, ThingPlaceMode.Near);
+        }
+    }
+}
