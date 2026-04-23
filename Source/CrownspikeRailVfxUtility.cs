7deb4917a177534fc61b8d3be4f0c929397d20ee
Neversalimus <neversalim@yandex.ru>
2026-04-24 01:30:18 +0300

    Add files via upload

--- Source/CrownspikeRailVfxUtility.cs
+++ Source/CrownspikeRailVfxUtility.cs
@@ -9,28 +9,37 @@ namespace AbyssalProtocol
         private const string RailBeamThingDefName = "ABY_Mote_CrownspikeRailBeam";
         private const string RailHaloThingDefName = "ABY_Mote_CrownspikeRailHalo";
         private const string RailCoreThingDefName = "ABY_Mote_CrownspikeRailCoreBeam";
+        private const string RailAfterimageThingDefName = "ABY_Mote_CrownspikeRailAfterimage";
         private const string MuzzleMoteDefName = "ABY_Mote_CrownspikeRailMuzzle";
+        private const string ChargeMoteDefName = "ABY_Mote_CrownspikeRailCharge";
         private const string ImpactMoteDefName = "ABY_Mote_CrownspikeRailImpact";
         private const string ShieldMoteDefName = "ABY_Mote_CrownspikeRailShieldImpact";
         private const string DenseImpactMoteDefName = "ABY_Mote_CrownspikeRailDenseImpact";
+        private const string ExecutionMoteDefName = "ABY_Mote_CrownspikeRailExecution";
 
         private static ThingDef railBeamDef;
         private static ThingDef railHaloDef;
         private static ThingDef railCoreDef;
+        private static ThingDef railAfterimageDef;
         private static ThingDef muzzleMoteDef;
+        private static ThingDef chargeMoteDef;
         private static ThingDef impactMoteDef;
         private static ThingDef shieldMoteDef;
         private static ThingDef denseImpactMoteDef;
+        private static ThingDef executionMoteDef;
 
         private static ThingDef RailBeamDef => railBeamDef ?? (railBeamDef = DefDatabase<ThingDef>.GetNamedSilentFail(RailBeamThingDefName));
         private static ThingDef RailHaloDef => railHaloDef ?? (railHaloDef = DefDatabase<ThingDef>.GetNamedSilentFail(RailHaloThingDefName));
         private static ThingDef RailCoreDef => railCoreDef ?? (railCoreDef = DefDatabase<ThingDef>.GetNamedSilentFail(RailCoreThingDefName));
+        private static ThingDef RailAfterimageDef => railAfterimageDef ?? (railAfterimageDef = DefDatabase<ThingDef>.GetNamedSilentFail(RailAfterimageThingDefName));
         private static ThingDef MuzzleMoteDef => muzzleMoteDef ?? (muzzleMoteDef = DefDatabase<ThingDef>.GetNamedSilentFail(MuzzleMoteDefName));
+        private static ThingDef ChargeMoteDef => chargeMoteDef ?? (chargeMoteDef = DefDatabase<ThingDef>.GetNamedSilentFail(ChargeMoteDefName));
         private static ThingDef ImpactMoteDef => impactMoteDef ?? (impactMoteDef = DefDatabase<ThingDef>.GetNamedSilentFail(ImpactMoteDefName));
         private static ThingDef ShieldMoteDef => shieldMoteDef ?? (shieldMoteDef = DefDatabase<ThingDef>.GetNamedSilentFail(ShieldMoteDefName));
         private static ThingDef DenseImpactMoteDef => denseImpactMoteDef ?? (denseImpactMoteDef = DefDatabase<ThingDef>.GetNamedSilentFail(DenseImpactMoteDefName));
+        private static ThingDef ExecutionMoteDef => executionMoteDef ?? (executionMoteDef = DefDatabase<ThingDef>.GetNamedSilentFail(ExecutionMoteDefName));
 
-        public static void SpawnRailDischarge(Vector3 source, Vector3 target, Map map, Thing hitThing, bool blockedByShield)
+        public static void SpawnRailDischarge(Vector3 source, Vector3 target, Map map, Thing hitThing, bool blockedByShield, bool executionPulse)
         {
             if (map == null)
             {
@@ -43,6 +52,102 @@ namespace AbyssalProtocol
             SpawnMuzzleFlash(source, target, map);
             SpawnRailLine(source, target, map);
             SpawnImpact(target, map, hitThing, blockedByShield);
+
+            if (executionPulse)
+            {
+                SpawnExecutionFlare(target, map);
+            }
+        }
+
+        public static void SpawnChargeAt(Vector3 source, Map map)
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            source.y = AltitudeLayer.MoteOverhead.AltitudeFor();
+
+            ThingDef chargeDef = ChargeMoteDef;
+            if (chargeDef != null)
+            {
+                MakeStaticMote(source, map, chargeDef, 1.12f);
+                MakeStaticMote(source + new Vector3(Rand.Range(-0.07f, 0.07f), 0f, Rand.Range(-0.07f, 0.07f)), map, chargeDef, 0.58f);
+            }
+
+            FleckMaker.ThrowLightningGlow(source, map, 1.15f);
+            if (Rand.Chance(0.70f))
+            {
+                FleckMaker.ThrowMicroSparks(source, map);
+            }
+        }
+
+        public static void SpawnShieldReaction(Vector3 position, Map map)
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            MakeStaticMote(position, map, ShieldMoteDef, 1.78f);
+            FleckMaker.ThrowLightningGlow(position, map, 2.85f);
+            FleckMaker.ThrowMicroSparks(position, map);
+            FleckMaker.ThrowMicroSparks(position, map);
+        }
+
+        public static void SpawnDenseResonance(Vector3 position, Map map, bool fromPierce)
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            MakeStaticMote(position, map, DenseImpactMoteDef, fromPierce ? 1.05f : 1.62f);
+            FleckMaker.ThrowLightningGlow(position, map, fromPierce ? 1.55f : 2.55f);
+            FleckMaker.ThrowMicroSparks(position, map);
+            FleckMaker.ThrowMicroSparks(position, map);
+            if (!fromPierce)
+            {
+                FleckMaker.ThrowMicroSparks(position, map);
+            }
+        }
+
+        public static void SpawnPierceImpact(Vector3 position, Map map, Thing hitThing, int pierceIndex)
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            bool denseTarget = IsDenseTarget(hitThing);
+            ThingDef impactDef = denseTarget ? DenseImpactMoteDef : ImpactMoteDef;
+            float scale = denseTarget ? 1.18f : 0.92f;
+            if (pierceIndex > 0)
+            {
+                scale *= 0.76f;
+            }
+
+            MakeStaticMote(position, map, impactDef, scale);
+            FleckMaker.ThrowLightningGlow(position, map, denseTarget ? 1.75f : 1.08f);
+            if (Rand.Chance(0.75f))
+            {
+                FleckMaker.ThrowMicroSparks(position, map);
+            }
+        }
+
+        public static void SpawnExecutionFlare(Vector3 position, Map map)
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            MakeStaticMote(position, map, ExecutionMoteDef, 1.96f);
+            MakeStaticMote(position + new Vector3(Rand.Range(-0.08f, 0.08f), 0f, Rand.Range(-0.08f, 0.08f)), map, ImpactMoteDef, 1.18f);
+            FleckMaker.ThrowLightningGlow(position, map, 3.10f);
+            FleckMaker.ThrowMicroSparks(position, map);
+            FleckMaker.ThrowMicroSparks(position, map);
+            FleckMaker.ThrowMicroSparks(position, map);
         }
 
         private static void SpawnMuzzleFlash(Vector3 source, Vector3 target, Map map)
@@ -50,15 +155,15 @@ namespace AbyssalProtocol
             ThingDef muzzleDef = MuzzleMoteDef;
             if (muzzleDef != null)
             {
-                MakeStaticMote(source, map, muzzleDef, 1.62f);
-                MakeStaticMote(Vector3.Lerp(source, target, 0.028f), map, muzzleDef, 0.92f);
-                MakeStaticMote(Vector3.Lerp(source, target, 0.058f), map, muzzleDef, 0.48f);
+                MakeStaticMote(source, map, muzzleDef, 1.78f);
+                MakeStaticMote(Vector3.Lerp(source, target, 0.028f), map, muzzleDef, 1.02f);
+                MakeStaticMote(Vector3.Lerp(source, target, 0.058f), map, muzzleDef, 0.56f);
             }
 
-            FleckMaker.ThrowLightningGlow(source, map, 2.25f);
+            FleckMaker.ThrowLightningGlow(source, map, 2.55f);
             FleckMaker.ThrowMicroSparks(source, map);
             FleckMaker.ThrowMicroSparks(source, map);
-            if (Rand.Chance(0.55f))
+            if (Rand.Chance(0.72f))
             {
                 FleckMaker.ThrowMicroSparks(source, map);
             }
@@ -73,21 +178,22 @@ namespace AbyssalProtocol
                 return;
             }
 
-            SpawnBeamThing(RailHaloDef, source, target, map, 0.96f, 8, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_BeamHalo", true);
-            SpawnBeamThing(RailBeamDef, source, target, map, 0.48f, 7, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_BeamGlow", true);
-            SpawnBeamThing(RailCoreDef, source, target, map, 0.16f, 5, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_BeamCore", false);
+            SpawnBeamThing(RailAfterimageDef, source, target, map, 0.30f, 18, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_Afterimage", false);
+            SpawnBeamThing(RailHaloDef, source, target, map, 1.02f, 8, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_BeamHalo", true);
+            SpawnBeamThing(RailBeamDef, source, target, map, 0.50f, 7, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_BeamGlow", true);
+            SpawnBeamThing(RailCoreDef, source, target, map, 0.14f, 5, "Things/VFX/CrownspikeRail/ABY_CrownspikeRail_BeamCore", false);
 
-            int sparkleSteps = Mathf.Clamp(Mathf.CeilToInt(distance * 0.42f), 3, 13);
+            int sparkleSteps = Mathf.Clamp(Mathf.CeilToInt(distance * 0.32f), 2, 9);
             for (int i = 1; i <= sparkleSteps; i++)
             {
                 float t = i / (float)(sparkleSteps + 1);
                 Vector3 point = Vector3.Lerp(source, target, t);
-                point += new Vector3(Rand.Range(-0.045f, 0.045f), 0f, Rand.Range(-0.045f, 0.045f));
+                point += new Vector3(Rand.Range(-0.035f, 0.035f), 0f, Rand.Range(-0.035f, 0.035f));
                 point.y = AltitudeLayer.MoteOverhead.AltitudeFor();
 
-                if (i % 2 == 0)
+                if (i % 2 == 0 || Rand.Chance(0.35f))
                 {
-                    FleckMaker.ThrowLightningGlow(point, map, 0.50f);
+                    FleckMaker.ThrowLightningGlow(point, map, 0.42f);
                 }
             }
         }
@@ -96,15 +202,15 @@ namespace AbyssalProtocol
         {
             bool denseTarget = IsDenseTarget(hitThing);
             ThingDef impactDef = blockedByShield ? ShieldMoteDef : denseTarget ? DenseImpactMoteDef : ImpactMoteDef;
-            float baseScale = blockedByShield ? 1.48f : denseTarget ? 1.74f : 1.34f;
+            float baseScale = blockedByShield ? 1.58f : denseTarget ? 1.84f : 1.38f;
 
             if (impactDef != null)
             {
                 MakeStaticMote(target, map, impactDef, baseScale);
-                MakeStaticMote(target + new Vector3(Rand.Range(-0.10f, 0.10f), 0f, Rand.Range(-0.10f, 0.10f)), map, impactDef, baseScale * 0.55f);
+                MakeStaticMote(target + new Vector3(Rand.Range(-0.10f, 0.10f), 0f, Rand.Range(-0.10f, 0.10f)), map, impactDef, baseScale * 0.50f);
             }
 
-            FleckMaker.ThrowLightningGlow(target, map, blockedByShield ? 2.40f : denseTarget ? 2.85f : 2.05f);
+            FleckMaker.ThrowLightningGlow(target, map, blockedByShield ? 2.60f : denseTarget ? 3.05f : 2.15f);
             FleckMaker.ThrowMicroSparks(target, map);
             FleckMaker.ThrowMicroSparks(target, map);
             if (blockedByShield || denseTarget)
@@ -152,7 +258,7 @@ namespace AbyssalProtocol
             GenSpawn.Spawn(beam, spawnCell, map);
         }
 
-        private static bool IsDenseTarget(Thing hitThing)
+        public static bool IsDenseTarget(Thing hitThing)
         {
             if (hitThing == null || hitThing.Destroyed)
             {
--- Source/Projectile_CrownspikeRailBolt.cs
+++ Source/Projectile_CrownspikeRailBolt.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -6,6 +7,19 @@ namespace AbyssalProtocol
 {
     public class Projectile_CrownspikeRailBolt : Bullet
     {
+        private const int MaxPierceTargets = 2;
+        private const float PierceReachBeyondImpact = 13.5f;
+        private const float PierceSampleStep = 0.42f;
+        private const float PierceLineRadius = 0.54f;
+        private const int BasePierceDamage = 62;
+        private const float BasePierceArmorPenetration = 1.45f;
+        private const float FirstPierceDamageMultiplier = 0.58f;
+        private const float SecondPierceDamageMultiplier = 0.38f;
+        private const float PierceArmorPenetrationMultiplier = 0.82f;
+        private const float DenseDirectEmpDamage = 6.0f;
+        private const float DensePierceEmpDamage = 3.5f;
+        private const float DenseStructurePulseDamage = 8.0f;
+
         private bool preImpactFlashDone;
 
         protected override void Tick()
@@ -17,7 +31,7 @@ namespace AbyssalProtocol
                 Vector3 forwardPoint = ExactPosition;
                 if ((forwardPoint - source).MagnitudeHorizontal() > 0.15f)
                 {
-                    CrownspikeRailVfxUtility.SpawnRailDischarge(source, forwardPoint, Map, null, false);
+                    CrownspikeRailVfxUtility.SpawnRailDischarge(source, forwardPoint, Map, null, false, false);
                 }
             }
 
@@ -27,8 +41,14 @@ namespace AbyssalProtocol
         protected override void Impact(Thing hitThing, bool blockedByShield = false)
         {
             Map impactMap = Map;
+            IntVec3 impactCell = Position;
             Vector3 impactPosition = ExactPosition;
             Vector3 source = ResolveSourcePosition();
+            Vector3 finalDestination = ResolveFinalDestination(source, impactPosition);
+            Thing instigator = Launcher;
+            bool directTargetWasDense = CrownspikeRailVfxUtility.IsDenseTarget(hitThing);
+            Pawn directPawn = hitThing as Pawn;
+            bool directPawnWasAlive = directPawn != null && !directPawn.Dead && !directPawn.Destroyed;
 
             base.Impact(hitThing, blockedByShield);
 
@@ -37,7 +57,250 @@ namespace AbyssalProtocol
                 return;
             }
 
-            CrownspikeRailVfxUtility.SpawnRailDischarge(source, impactPosition, impactMap, hitThing, blockedByShield);
+            CrownspikeRailVfxUtility.SpawnRailDischarge(source, impactPosition, impactMap, hitThing, blockedByShield, false);
+
+            if (blockedByShield)
+            {
+                CrownspikeRailVfxUtility.SpawnShieldReaction(impactPosition, impactMap);
+                return;
+            }
+
+            if (directTargetWasDense)
+            {
+                ApplyDenseResonance(hitThing, instigator, impactPosition, impactMap, false);
+            }
+
+            if (directPawnWasAlive && directPawn != null && (directPawn.Dead || directPawn.Destroyed))
+            {
+                CrownspikeRailVfxUtility.SpawnExecutionFlare(impactPosition, impactMap);
+            }
+
+            ApplyPierceLine(source, impactPosition, finalDestination, impactMap, hitThing, instigator, impactCell);
+        }
+
+        private void ApplyPierceLine(Vector3 source, Vector3 impactPosition, Vector3 finalDestination, Map map, Thing primaryHitThing, Thing instigator, IntVec3 impactCell)
+        {
+            if (map == null)
+            {
+                return;
+            }
+
+            Vector3 direction = finalDestination - source;
+            direction.y = 0f;
+            if (direction.MagnitudeHorizontalSquared() < 0.001f)
+            {
+                direction = impactPosition - source;
+                direction.y = 0f;
+            }
+            if (direction.MagnitudeHorizontalSquared() < 0.001f)
+            {
+                return;
+            }
+
+            direction.Normalize();
+
+            float startDistance = Mathf.Max(0f, DistanceAlongLine(source, direction, impactPosition) + 0.38f);
+            float endDistance = startDistance + PierceReachBeyondImpact;
+
+            List<PierceCandidate> candidates = new List<PierceCandidate>();
+            HashSet<int> seenThingIds = new HashSet<int>();
+            HashSet<IntVec3> sampledCells = new HashSet<IntVec3>();
+
+            for (float distance = startDistance; distance <= endDistance; distance += PierceSampleStep)
+            {
+                Vector3 sample = source + direction * distance;
+                IntVec3 centerCell = sample.ToIntVec3();
+                for (int ox = -1; ox <= 1; ox++)
+                {
+                    for (int oz = -1; oz <= 1; oz++)
+                    {
+                        IntVec3 cell = new IntVec3(centerCell.x + ox, centerCell.y, centerCell.z + oz);
+                        if (!cell.InBounds(map) || sampledCells.Contains(cell))
+                        {
+                            continue;
+                        }
+
+                        sampledCells.Add(cell);
+                        List<Thing> things = cell.GetThingList(map);
+                        for (int i = 0; i < things.Count; i++)
+                        {
+                            Thing thing = things[i];
+                            if (!IsValidPierceTarget(thing, primaryHitThing, instigator, seenThingIds))
+                            {
+                                continue;
+                            }
+
+                            Vector3 thingPosition = thing.DrawPos;
+                            float along = DistanceAlongLine(source, direction, thingPosition);
+                            if (along < startDistance - 0.15f || along > endDistance + 0.15f)
+                            {
+                                continue;
+                            }
+
+                            float lateral = LateralDistanceToLine(source, direction, thingPosition);
+                            if (lateral > PierceLineRadius + thing.def.fillPercent * 0.38f)
+                            {
+                                continue;
+                            }
+
+                            seenThingIds.Add(thing.thingIDNumber);
+                            candidates.Add(new PierceCandidate(thing, along, lateral));
+                        }
+                    }
+                }
+            }
+
+            if (candidates.Count == 0)
+            {
+                return;
+            }
+
+            candidates.Sort((a, b) =>
+            {
+                int byDistance = a.distance.CompareTo(b.distance);
+                return byDistance != 0 ? byDistance : a.lateralDistance.CompareTo(b.lateralDistance);
+            });
+
+            int affected = 0;
+            for (int i = 0; i < candidates.Count && affected < MaxPierceTargets; i++)
+            {
+                Thing target = candidates[i].thing;
+                if (target == null || target.Destroyed)
+                {
+                    continue;
+                }
+
+                float damageMultiplier = affected == 0 ? FirstPierceDamageMultiplier : SecondPierceDamageMultiplier;
+                ApplyPierceDamage(target, instigator, damageMultiplier);
+
+                Vector3 targetPos = target.DrawPos;
+                CrownspikeRailVfxUtility.SpawnPierceImpact(targetPos, map, target, affected);
+
+                if (CrownspikeRailVfxUtility.IsDenseTarget(target))
+                {
+                    ApplyDenseResonance(target, instigator, targetPos, map, true);
+                }
+
+                affected++;
+            }
+        }
+
+        private void ApplyPierceDamage(Thing target, Thing instigator, float damageMultiplier)
+        {
+            if (target == null || target.Destroyed)
+            {
+                return;
+            }
+
+            int damage = Mathf.Max(1, Mathf.RoundToInt(BasePierceDamage * damageMultiplier));
+            float armorPenetration = BasePierceArmorPenetration * PierceArmorPenetrationMultiplier;
+
+            DamageInfo damageInfo = new DamageInfo(
+                DamageDefOf.Bullet,
+                damage,
+                armorPenetration,
+                -1f,
+                instigator,
+                null,
+                null,
+                DamageInfo.SourceCategory.ThingOrUnknown);
+
+            target.TakeDamage(damageInfo);
+        }
+
+        private static void ApplyDenseResonance(Thing target, Thing instigator, Vector3 position, Map map, bool fromPierce)
+        {
+            if (target == null || target.Destroyed || map == null)
+            {
+                return;
+            }
+
+            bool dense = CrownspikeRailVfxUtility.IsDenseTarget(target);
+            if (!dense)
+            {
+                return;
+            }
+
+            float empDamage = fromPierce ? DensePierceEmpDamage : DenseDirectEmpDamage;
+            target.TakeDamage(new DamageInfo(
+                DamageDefOf.EMP,
+                empDamage,
+                0f,
+                -1f,
+                instigator,
+                null,
+                null,
+                DamageInfo.SourceCategory.ThingOrUnknown));
+
+            Building building = target as Building;
+            if (building != null && building.def != null && building.def.useHitPoints)
+            {
+                building.TakeDamage(new DamageInfo(
+                    DamageDefOf.Bomb,
+                    fromPierce ? DenseStructurePulseDamage * 0.55f : DenseStructurePulseDamage,
+                    0.35f,
+                    -1f,
+                    instigator,
+                    null,
+                    null,
+                    DamageInfo.SourceCategory.ThingOrUnknown));
+            }
+
+            CrownspikeRailVfxUtility.SpawnDenseResonance(position, map, fromPierce);
+        }
+
+        private static bool IsValidPierceTarget(Thing thing, Thing primaryHitThing, Thing instigator, HashSet<int> seenThingIds)
+        {
+            if (thing == null || thing.Destroyed || !thing.Spawned || thing == primaryHitThing || thing == instigator)
+            {
+                return false;
+            }
+
+            if (seenThingIds.Contains(thing.thingIDNumber))
+            {
+                return false;
+            }
+
+            if (thing is Mote || thing is Filth || thing is Blueprint || thing is Frame)
+            {
+                return false;
+            }
+
+            Faction instigatorFaction = instigator != null ? instigator.Faction : null;
+
+            Pawn pawn = thing as Pawn;
+            if (pawn != null)
+            {
+                if (pawn.Dead || pawn.Downed && pawn.RaceProps != null && pawn.RaceProps.Animal)
+                {
+                    return false;
+                }
+
+                if (instigatorFaction != null && pawn.Faction == instigatorFaction)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            Building building = thing as Building;
+            if (building != null)
+            {
+                if (building.def == null || !building.def.useHitPoints)
+                {
+                    return false;
+                }
+
+                if (instigatorFaction != null && building.Faction == instigatorFaction)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            return false;
         }
 
         private Vector3 ResolveSourcePosition()
@@ -49,6 +312,49 @@ namespace AbyssalProtocol
             }
 
             return ExactPosition;
+        }
+
+        private Vector3 ResolveFinalDestination(Vector3 source, Vector3 impactPosition)
+        {
+            Vector3 direction = impactPosition - source;
+            direction.y = 0f;
+            if (direction.MagnitudeHorizontalSquared() < 0.001f)
+            {
+                return impactPosition;
+            }
+
+            direction.Normalize();
+            return impactPosition + direction * PierceReachBeyondImpact;
+        }
+
+        private static float DistanceAlongLine(Vector3 origin, Vector3 normalizedDirection, Vector3 point)
+        {
+            Vector3 delta = point - origin;
+            delta.y = 0f;
+            return Vector3.Dot(delta, normalizedDirection);
+        }
+
+        private static float LateralDistanceToLine(Vector3 origin, Vector3 normalizedDirection, Vector3 point)
+        {
+            Vector3 delta = point - origin;
+            delta.y = 0f;
+            float along = Vector3.Dot(delta, normalizedDirection);
+            Vector3 projected = origin + normalizedDirection * along;
+            return (point - projected).MagnitudeHorizontal();
+        }
+
+        private struct PierceCandidate
+        {
+            public readonly Thing thing;
+            public readonly float distance;
+            public readonly float lateralDistance;
+
+            public PierceCandidate(Thing thing, float distance, float lateralDistance)
+            {
+                this.thing = thing;
+                this.distance = distance;
+                this.lateralDistance = lateralDistance;
+            }
         }
     }
 }
